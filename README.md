620cec689a8d03f6e227882df21b76d8d2fd7f5c
piwneuh <filip.pinjuh@gmail.com>
2024-09-02 14:50:26 +0200

    go pipe it

---
+++ README.md
@@ -0,0 +1,18 @@
+# GIPER
+
+Golang log piper - hence the name GIPER.
+
+Nothing special to see here please move along...
+
+### Use it ?
+
+1. Clone the repo
+2. Run `go mod tidy`
+3. Run `go install`
+4. Pipe golang logs to it like so :
+
+   ```bash
+   go run main.go | giper
+   ```
+
+5. Profit ??
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/fatih/color"
+)
+
+type LogEntry struct {
+	DateTime   string `json:"DateTime"`
+	LogLevel   string `json:"LogLevel"`
+	FileName   string `json:"FileName"`
+	MethodName string `json:"MethodName"`
+	LineNumber string `json:"LineNumber"`
+	LogMessage string `json:"LogMessage"`
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		var entry LogEntry
+		err := json.Unmarshal([]byte(scanner.Text()), &entry)
+		if err != nil {
+			fmt.Println(scanner.Text()) // Print non-JSON lines directly
+			continue
+		}
+
+		switch entry.LogLevel {
+		case "INFO":
+			color.New(color.FgGreen).Printf("[%s] %s - %s:%s %s\n", entry.LogLevel, entry.DateTime, entry.FileName, entry.LineNumber, entry.LogMessage)
+		case "DEBUG":
+			color.New(color.FgYellow).Printf("[%s] %s - %s:%s %s\n", entry.LogLevel, entry.DateTime, entry.FileName, entry.LineNumber, entry.LogMessage)
+		case "ERROR":
+			color.New(color.FgRed).Printf("[%s] %s - %s:%s %s\n", entry.LogLevel, entry.DateTime, entry.FileName, entry.LineNumber, entry.LogMessage)
+		default:
+			fmt.Printf("[%s] %s - %s:%s %s\n", entry.LogLevel, entry.DateTime, entry.FileName, entry.LineNumber, entry.LogMessage)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
